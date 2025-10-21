fcc34dcd2e92b89720918a433cfa40bc68bb4a42
Vinitka's Dinasty <145179274+vinitkanov@users.noreply.github.com>
2025-10-21 10:46:58 +0700

    Edit index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,46 @@
+import { Text, View, TextInput } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>App Data Diri Saya</Text>
+      <Text>Nama</Text>
+      <TextInput
+        placeholder="Miqdad"
+        style={{
+          height: 40,
+          padding: 5,
+          marginHorizontal: 8,
+          borderWidth: 1,
+        }}
+      />
+      <Text>Kelas</Text>
+      <TextInput
+        placeholder="11"
+        style={{
+          height: 40,
+          padding: 5,
+          marginHorizontal: 8,
+          borderWidth: 1,
+        }}
+      />
+      <Text>Hobi</Text>
+      <TextInput
+        placeholder="Ngopi"
+        style={{
+          height: 40,
+          padding: 5,
+          marginHorizontal: 8,
+          borderWidth: 1,
+        }}
+      />
+      <Text>Terima Kasih sudah mengisi form!</Text>
+    </View>
+  );
+}
